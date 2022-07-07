b8909420434bc919810b54e4de65819d79848f40
Setivyn <daniel@flightless.co.uk>
2022-07-07 13:46:47 +0100

    Pre-Game & Close Down Completed
    During Pre-Game, SPACE Starts Sim,
    BACKSPACE Clears Canvas.

--- GoL/Main.c
+++ GoL/Main.c
@@ -8,67 +8,25 @@
 //playSpace Definition
 struct playSpace
 {
-    int size;
+    Uint32 size;
     bool* cells;
     bool stable;
 };
 typedef struct playSpace playSpace;
 
-playSpace* playSpaceCreate(int size)
+playSpace* playSpaceCreate(Uint32 size)
 {
     playSpace* grid = malloc(sizeof(*grid));
-    int N = size;
 
     grid->size = size;
-    grid->cells = calloc(N * N, sizeof(bool));
+    grid->cells = calloc(size * size, sizeof(bool));
     grid->stable = false;
 
     return grid;
 }
 
 
-bool initGame(SDL_Window* Display, SDL_Renderer* renderer, playSpace* state, playSpace* state0, int size)
-{
-    //Initialise SDL Window
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-    {
-        return false;
-    }
-
-    Display = SDL_CreateWindow("Game of Life", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
-
-    if (Display == NULL)
-    {
-        return false;
-    }
-
-    renderer = SDL_CreateRenderer(Display, -1, 0);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderPresent(renderer);
-
-    //
-    //Initialise PlaySpace
-
-    state = playSpaceCreate(size);
-    state0 = NULL;
-
-    if (state == NULL)
-    {
-        return false;
-    }
-
-    return true;
-}
-
 //Define and Free playSpace structures
-
-SDL_Surface initDisplay(int size)
-{
-
-
-}
 
 void playSpaceFree(playSpace* space)
 {
@@ -79,28 +37,122 @@ void playSpaceFree(playSpace* space)
 
 int main()
 {
-    int gridSize = 100;
+    Uint32 gridSize = 1000;
     SDL_Window* Game_Display;
-    SDL_Renderer* renderer;
+    SDL_Renderer* Renderer;
 
     playSpace* state;
     playSpace* state0;
 
-    if (initGame(&Game_Display, &renderer, &state, &state0, gridSize))
+    bool LMBDown = false;
+
+    int mousex;
+    int mousey;
+
+    //Initialise GameSpaces & Rendering
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    Game_Display = SDL_CreateWindow("Game of Life", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, gridSize, gridSize, 0);
+    Renderer = SDL_CreateRenderer(Game_Display, -1, 0);
+    SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 0);
+    SDL_RenderClear(Renderer);
+    SDL_RenderPresent(Renderer);
+
+    state = playSpaceCreate(gridSize);
+    state0 = NULL;
+    
+
+    SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 255);
+
+    //
+
+
+    //Main Game Loop
+    
+    SDL_Event event;
+    bool quit = false;
+    int GameState = 0;
+
+    while (!quit)
     {
-        bool close = false;
-
-        while (!close) {
-            SDL_Event event;
-
-            while (SDL_PollEvent(&event)) {
-                switch (event.type) {
-                case SDL_QUIT:
-                    close = true;
+        //Quit Logic
+        SDL_PollEvent(&event);
+        if (event.type == SDL_QUIT) { quit = true; break; }
+        
+        //Game Logic
+        switch (GameState)
+        {
+            //Pre-Game
+            case 0:
+            {
+                switch (event.type)
+                {
+                case SDL_MOUSEBUTTONUP:
+                    if (event.button.button == SDL_BUTTON_LEFT)
+                        LMBDown = false;
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    if (event.button.button == SDL_BUTTON_LEFT)
+                        LMBDown = true;
+                case SDL_MOUSEMOTION:
+                    if (LMBDown)
+                    {
+                        mousex = event.motion.x;
+                        mousey = event.motion.y;
+                        SDL_RenderDrawPoint(Renderer, mousex, mousey);
+                    }
                     break;
                 }
+                case SDL_KEYDOWN:
+                {
+                    if (event.key.keysym.sym == SDLK_SPACE)
+                    {
+                        GameState = 1;
+                        break;
+                    }
+                    if (event.key.keysym.sym == SDLK_BACKSPACE)
+                    {
+                        SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 0);
+                        SDL_RenderClear(Renderer);
+                        SDL_RenderPresent(Renderer);
+                        SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 255);
+                        break;
+                    }
+                }
+            }
+
+            //Mid-Game
+            case 1:
+            {
+
+            }
+
+            //Post-Game
+            case 2:
+            {
+
             }
         }
+
+        SDL_RenderPresent(Renderer);
     }
+    //
+    
+    //Quit Clean-up
+    SDL_DestroyRenderer(Renderer);
+    SDL_DestroyWindow(Game_Display);
+
+    SDL_Quit();
+    playSpaceFree(state);
+    playSpaceFree(state0);
+
+    free(gridSize);
+    free(LMBDown);
+    free(mousex);
+    free(mousey);
+    free(quit);
+    free(GameState);
+
+    //
     return 0;
 }
